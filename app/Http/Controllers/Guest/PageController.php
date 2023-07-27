44597c8a1b30516362b9d17ba067d1b200e38007
quintusdev <quintusdev92@gmail.com>
2023-07-27 16:14:43 +0200

    ES. BASE COMPLETATO

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Comics;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $comics = Comics::all();
+
+        return view('home', compact('comics'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,18 +16,27 @@
 </head>
 
 <body>
-
-    <main class="bg-light">
+    <div class="bg-dark">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-
+                <h1 class="fw-bold text-white text-center">COMICS</h1>
+                <div class="col-12 d-flex flex-wrap">
+                    @forelse ($comics as $comic)
+                        <div class="col-3 p-4 d-flex justify-content-center m-5">
+                            <div class="card">
+                                <img src="{{ $comic->thumb }}" alt="">
+                                <h5>{{ $comic->series }}</h5>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="col-12">
+                            Sorry no COMIC found!
+                        </div>
+                    @endforelse
                 </div>
             </div>
-
         </div>
-    </main>
-
+    </div>
 </body>
 
 </html>
